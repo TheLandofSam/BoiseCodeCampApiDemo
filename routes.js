2e797d208fab71f18aa8529bd939c62f6a3ab43b
Daniel Breen <daninreallife7@gmail.com>
2017-07-12 12:16:27 -0600

    Remove / Response

--- routes.js
+++ routes.js
@@ -3,7 +3,3 @@ var server = require('./server');
 server.get('/api/v1/posts', (req, res, next) => {
     res.send(200, 'Hello World!');
 });
-
-server.get('/', (req, res, next) => {
-    res.send(200, 'Hello World!');
-});
