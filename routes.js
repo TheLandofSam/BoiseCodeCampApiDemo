ef32d9985c957e4c7e63997c3844d14c9b5ef770
Daniel Breen <daninreallife7@gmail.com>
2017-07-12 11:49:05 -0600

    Change Entry Point

--- routes.js
+++ routes.js
@@ -3,3 +3,7 @@ var server = require('./server');
 server.get('/api/v1/posts', (req, res, next) => {
     res.send(200, 'Hello World!');
 });
+
+server.get('/', (req, res, next) => {
+    res.send(200, 'Hello World!');
+});
