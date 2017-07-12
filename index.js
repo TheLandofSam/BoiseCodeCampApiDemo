d0df2e1d1da065eda22ce8ba84b1ebdff0bffce7
Daniel Breen <daninreallife7@gmail.com>
2017-07-12 13:02:33 -0600

    Use Heroku port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('./models');
 var restify = require('restify');
-var port = 80;
+var port = 3000;
 
   var server = restify.createServer({
     name: 'Boise Code Camp Demo',
@@ -10,7 +10,7 @@ var port = 80;
   server.use(restify.plugins.acceptParser(server.acceptable));
   server.use(restify.plugins.queryParser());
   server.use(restify.plugins.bodyParser());
-  server.listen(port, function () {
+  server.listen(process.env.PORT || port, function () {
     console.log('%s listening at %s', server.name, server.url);
   });
 
