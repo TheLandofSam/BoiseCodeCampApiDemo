c2d48fa974d6f49d3b3314877b1bde29b6c89cc2
Daniel Breen <daninreallife7@gmail.com>
2017-07-13 12:20:56 -0600

    Scaffold postsController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,1 +1,22 @@
-var posts = require("../database/posts");
+let db = require('../models');
+let Post = db.Post;
+
+exports.index = (req, res, next) => {
+
+}
+
+exports.view = (req, res, next) => {
+
+}
+
+exports.update = (req, res, next) => {
+
+}
+
+exports.create = (req, res, next) => {
+
+}
+
+exports.remove = (req, res, next) => {
+
+}
